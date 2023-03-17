bc7bef8087b4ab4951f730bd49e345229d365125
N4D1K-lgtm <77400755+N4D1K-lgtm@users.noreply.github.com>
2023-03-16 21:46:59 -0400

    Replaced Node class with public NavGridNode

---
+++ Assets/_Scripts/Pathfinding/NavGridNode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+// Implement the Nodes
+public class NavGridNode : IComparable<NavGridNode>
+{
+    // Each node has a position on the tilemap (this is equal to its key inside the dictionary)
+    public Vector3Int position;
+
+    // Initialize g, h and f cost, initialize a null parent reference to be used to return the path
+    public bool traversable;
+    public int gCost;
+    public int hCost;
+    public int fCost { get { return gCost + hCost; } }
+
+    public NavGridNode parent;
+
+    // Default empty Node
+    public NavGridNode(Vector3Int pos, bool traverseabl)
+    {
+        traversable = traverseabl;
+        position = pos;
+        gCost = 0;
+        hCost = 0;
+        parent = null;
+    }
+
+    // CompareTo function for Min, Max functions
+    public int CompareTo(NavGridNode other)
+    {
+        if (fCost < other.fCost)
+        {
+            return -1;
+        }
+        else if (fCost > other.fCost)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
